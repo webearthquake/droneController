46aedb7173046fa8c8633ec12778c344898d02d3
webearthquake <105305368+webearthquake@users.noreply.github.com>
2022-05-16 08:18:19 -0700

    Create telloDroneController.py

---
+++ telloDroneController.py
@@ -0,0 +1,29 @@
+from djitellopy import Tello as tello
+import cv2
+
+tello.connect()
+#set at right height
+tello.takeoff()
+tello.move_up(100)
+#start forever loop
+while True:
+    keypress = cv2.waitKey(0.5) & 0xff
+    #if esc key pressed, stop forever loop
+    if keypress == 27:
+        break
+    #WASD movement controls
+    elif keypress == ord('w'):
+        tello.move_forward(25)
+    elif keypress == ord('a'):
+        tello.move_left(25)
+    elif keypress == ord('s'):
+        tello.move_back(25)
+    elif keypress == ord('d'):
+        tello.move_right(25)
+        #E and Q to rotate drone
+    elif keypress == ord('q'):
+        tello.rotate_counter_clockwise(15)
+    elif keypress == ord('e'):
+        tello.rotate_clockwise(15)
+#land when loop broken
+tello.land()
